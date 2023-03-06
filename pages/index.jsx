fbbc625e46bdae3de31f32acb1ba2d0ac4c2a379
Fidesnoella <fniragena@gmail.com>
2023-03-06 08:19:40 +0200

    chore: create development branch

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='py-10 text-3xl text-center'>University of Amsterdam</h1>
+      <h1 className='py-10 text-3xl text-center text-cyan-600'>University of Amsterdam</h1>
     </>
   )
 }
